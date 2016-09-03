ddea9d388b92090461c5cb1f806710cbf2683df5
Paul Davies <paul_t_d@hotmail.com>
2016-09-03 10:52:15 +0100

    Add spot conflict check.

--- ActsInTown-domain/src/uk/co/luciditysoftware/actsintown/domain/entities/Spot.java
+++ ActsInTown-domain/src/uk/co/luciditysoftware/actsintown/domain/entities/Spot.java
@@ -1,9 +1,15 @@
 package uk.co.luciditysoftware.actsintown.domain.entities;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.UUID;
 
 import uk.co.luciditysoftware.actsintown.domain.common.Entity;
+import uk.co.luciditysoftware.actsintown.domain.common.ValidationMessage;
+import uk.co.luciditysoftware.actsintown.domain.common.ValidationMessageType;
 import uk.co.luciditysoftware.actsintown.domain.parametersets.spot.AddParameterSet;
 
 public class Spot extends Entity  {
@@ -69,6 +75,53 @@ public class Spot extends Entity  {
 	
 	public void setCancelled(boolean cancelled) {
 		this.cancelled = cancelled;
+	}
+	
+	public Date getScheduledEnd() {
+		GregorianCalendar cal = new GregorianCalendar();
+        cal.setTime(scheduledFor);
+        cal.add(Calendar.MINUTE, durationMinutes);        
+        Date scheduledEnd = cal.getTime();
+        return scheduledEnd;
+	}
+
+	public static List<ValidationMessage> validateAdd(AddParameterSet parameterSet) {
+		List<ValidationMessage> validationMessages = new ArrayList<ValidationMessage>();
+		
+		Date now = new Date();
+
+		if (parameterSet.getScheduledFor() == null) {
+			validationMessages.add(new ValidationMessage() {
+				{
+					setType(ValidationMessageType.ERROR);
+					setField("scheduledFor");
+					setText("Scheduled for is required.");
+				}
+			});
+		} else if (parameterSet.getScheduledFor().before(now)) {
+			validationMessages.add(new ValidationMessage() {
+				{
+					setType(ValidationMessageType.ERROR);
+					setField("scheduledFor");
+					setText("Scheduled for must not be in the past.");
+				}
+			});
+		}
+
+		//Ensure duration is psositve and > 0
+	
+		if (parameterSet.getUser() != null && parameterSet.getScheduledFor() != null 
+				&& parameterSet.getUser().getConflictingSpots(parameterSet.getScheduledFor(), parameterSet.getDurationMinutes()).size() > 0) {
+			validationMessages.add(new ValidationMessage() {
+				{
+					setType(ValidationMessageType.ERROR);
+					setField("");
+					setText("Spot conflicts with existing spots.");
+				}
+			});
+		}
+		
+		return validationMessages;
 	}
 	
 	public static Spot add(AddParameterSet parameterSet) {
--- ActsInTown-domain/src/uk/co/luciditysoftware/actsintown/domain/entities/User.java
+++ ActsInTown-domain/src/uk/co/luciditysoftware/actsintown/domain/entities/User.java
@@ -1,5 +1,11 @@
 package uk.co.luciditysoftware.actsintown.domain.entities;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.UUID;
 
 import uk.co.luciditysoftware.actsintown.domain.common.Entity;
@@ -14,6 +20,7 @@ public class User extends Entity {
 	private String email;
 	private boolean enabled;
 	private Role role;
+	private Collection<Spot> spots;
 	
 	public String getUsername() {
 		return username;
@@ -79,6 +86,34 @@ public class User extends Entity {
 		this.role = role;
 	}
 
+	public Collection<Spot> getSpots() {
+		return spots;
+	}
+
+	public void setSpots(Collection<Spot> spots) {
+		this.spots = spots;
+	}
+	
+	public List<Spot> getConflictingSpots(Date scheduledFor, int durationMinutes) {
+		List<Spot> conflictingSpots = new ArrayList<Spot>();
+		GregorianCalendar cal = new GregorianCalendar();
+        cal.setTime(scheduledFor);
+        cal.add(Calendar.MINUTE, durationMinutes);        
+        Date scheduledEnd = cal.getTime();
+        
+		for (Spot spot : spots) {
+			if ((scheduledEnd.after(spot.getScheduledFor()) || scheduledEnd.equals(spot.getScheduledFor()))
+					&& (scheduledEnd.before(spot.getScheduledEnd()) || scheduledEnd.equals(spot.getScheduledEnd()))) {
+				conflictingSpots.add(spot);
+			} else if ((scheduledFor.after(spot.getScheduledFor()) || scheduledFor.equals(spot.getScheduledFor()))
+					&& (scheduledFor.before(spot.getScheduledEnd()) || scheduledFor.equals(spot.getScheduledEnd()))) {
+				conflictingSpots.add(spot);
+			}
+		}
+
+		return conflictingSpots;
+	}
+	
 	public static User register(RegisterParameterSet parameterSet) {
 		User user = new User();
 		user.id = UUID.randomUUID();
