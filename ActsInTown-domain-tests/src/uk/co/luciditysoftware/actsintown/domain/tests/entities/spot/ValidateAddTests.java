2045a52e8f1074cc019f0b524349515c7bb812d3
Paul Davies <paul_t_d@hotmail.com>
2016-11-30 20:58:44 +0000

    Updating tab policy.

--- ActsInTown-domain-tests/src/uk/co/luciditysoftware/actsintown/domain/tests/entities/spot/ValidateAddTests.java
+++ ActsInTown-domain-tests/src/uk/co/luciditysoftware/actsintown/domain/tests/entities/spot/ValidateAddTests.java
@@ -18,62 +18,77 @@ import uk.co.luciditysoftware.actsintown.domain.entities.Town;
 import uk.co.luciditysoftware.actsintown.domain.entities.User;
 import uk.co.luciditysoftware.actsintown.domain.parametersets.spot.AddParameterSet;
 
+/**
+ * Test cases for the ValidateAdd method of the Spot entity.
+ * 
+ * @author Paul Davies
+ */
 public class ValidateAddTests {
-	private SimpleDateFormat dateFormat;
-	private User user;
+    private SimpleDateFormat dateFormat;
+    private User user;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
 
-	@Before
-	public void setUp() throws Exception {
-		dateFormat = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+    /**
+     * Sets up a User entity with a related Spot entity for running tests against.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        dateFormat = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
 
-		user = new User() {
-			{
-				setUsername("gary.green@green.com");
-				setSpots(new ArrayList<Spot>());
-			}
-		};
+        user = new User() {
+            {
+                setUsername("gary.green@green.com");
+                setSpots(new ArrayList<Spot>());
+            }
+        };
 
-		Spot existingSpot = new Spot() {
-			{
-				setUser(user);
-				setScheduledFor(dateFormat.parse("16-11-2020 19:30:00"));
-				setDurationMinutes(30);
-				setTown(new Town() {
-					{
-						this.setName("Blueville");
-					}
-				});
-				setVenueName("Test Venue 01");
-				setAddedOn(new Date());
-				setCancelled(false);
-			}
-		};
-		
-		user.getSpots().add(existingSpot);
-	}
-	
-	@Test
-	public void conflictingSpotReturnsValidationError() throws ParseException {
-		AddParameterSet parameterSet = new AddParameterSet() {
-			{
-				this.setUser(user);
-				this.setScheduledFor(dateFormat.parse("16-11-2020 19:45:00"));
-				this.setDurationMinutes(30);
-				this.setTown(new Town() {
-					{
-						this.setName("Greenville");
-					}
-				});
-				this.setVenueName("Test Venue 02");
-			}
-		};
+        Spot existingSpot = new Spot() {
+            {
+                setUser(user);
+                setScheduledFor(dateFormat.parse("16-11-2020 19:30:00"));
+                setDurationMinutes(30);
+                setTown(new Town() {
+                    {
+                        this.setName("Blueville");
+                    }
+                });
+                setVenueName("Test Venue 01");
+                setAddedOn(new Date());
+                setCancelled(false);
+            }
+        };
+        
+        user.getSpots().add(existingSpot);
+    }
+    
+    /**
+     * Tests that when a parameter set for adding a Spot is validated against a User, if the requested Spot conflicts
+     * with existing Spots, a validation error is returned.
+     * 
+     * @throws ParseException
+     */
+    @Test
+    public void conflictingSpotReturnsValidationError() throws ParseException {
+        AddParameterSet parameterSet = new AddParameterSet() {
+            {
+                this.setUser(user);
+                this.setScheduledFor(dateFormat.parse("16-11-2020 19:45:00"));
+                this.setDurationMinutes(30);
+                this.setTown(new Town() {
+                    {
+                        this.setName("Greenville");
+                    }
+                });
+                this.setVenueName("Test Venue 02");
+            }
+        };
 
-		List<ValidationMessage> validationMessages = Spot.validateAdd(parameterSet);
+        List<ValidationMessage> validationMessages = Spot.validateAdd(parameterSet);
 
-		assertEquals(1, validationMessages.size());
-	}
+        assertEquals(1, validationMessages.size());
+    }
 }
--- ActsInTown-domain-tests/src/uk/co/luciditysoftware/actsintown/domain/tests/entities/user/GetConflictingSpotsTests.java
+++ ActsInTown-domain-tests/src/uk/co/luciditysoftware/actsintown/domain/tests/entities/user/GetConflictingSpotsTests.java
@@ -15,201 +15,200 @@ import uk.co.luciditysoftware.actsintown.domain.entities.Spot;
 import uk.co.luciditysoftware.actsintown.domain.entities.Town;
 import uk.co.luciditysoftware.actsintown.domain.entities.User;
 
+/**
+ * Test cases for the GetConflictingSpots method of the User entity.
+ * 
+ * @author Paul Davies
+ */
 public class GetConflictingSpotsTests {
-	private User user;
-	private SimpleDateFormat dateFormat;
+    private User user;
+    private SimpleDateFormat dateFormat;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
 
-	@Before
-	public void setUp() throws Exception {
-		user = new User();
-		List<Spot> spots = new ArrayList<Spot>();
-		dateFormat = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+    /**
+     * Creates a collection of spots for running the various tests against.
+     * 
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        user = new User();
+        List<Spot> spots = new ArrayList<Spot>();
+        dateFormat = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
 
-		/*spots.add(new Spot() {
-			{
-				setScheduledFor(dateFormat.parse("16-11-2020 19:00:00"));
-				setDurationMinutes(30);
-				setTown(new Town());
-				setVenueName("Test Venue 01");
-				setAddedOn(new Date());
-				setCancelled(false);
-			}
-		});*/
+        spots.add(new Spot() {
+            {
+                setScheduledFor(dateFormat.parse("16-11-2020 19:30:00"));
+                setDurationMinutes(30);
+                setTown(new Town());
+                setVenueName("Test Venue 02");
+                setAddedOn(new Date());
+                setCancelled(false);
+            }
+        });
 
-		spots.add(new Spot() {
-			{
-				setScheduledFor(dateFormat.parse("16-11-2020 19:30:00"));
-				setDurationMinutes(30);
-				setTown(new Town());
-				setVenueName("Test Venue 02");
-				setAddedOn(new Date());
-				setCancelled(false);
-			}
-		});
+        user.setSpots(spots);
+    }
 
-		user.setSpots(spots);
-	}
+    /**
+     * Test the case when start and end are before the earlier
+     * booking. No conflicting bookings should be returned.
+     * 
+     * @throws ParseException
+     */
+    @Test
+    public void nothingReturnedIfBeforeEarliestBooking() throws ParseException {
+        // Act.
+        List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 18:00:00"), 30);
 
-	/**
-	 * Test the case when start and end are before the earlier
-	 * booking. No conflicting bookings should be returned.
-	 * 
-	 * @throws ParseException
-	 */
-	@Test
-	public void nothingReturnedIfBeforeEarliestBooking() throws ParseException {
-		// Act.
-		List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 18:00:00"), 30);
+        // Assert.
+        assertEquals(0, result.size());
+    }
 
-		// Assert.
-		assertEquals(0, result.size());
-	}
+    /**
+     * Test the case when start date is before start of a conflicting booking,
+     * and end date is equal to the start date. The conflicting booking should
+     * be returned.
+     * 
+     * @throws ParseException
+     */
+    @Test
+    public void returnsBookingIfStartDateBeforeAndEndDateEqualsStart() throws ParseException {
+        // Act.
+        List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 19:00:00"), 30);
 
-	/**
-	 * Test the case when start date is before start of a conflicting booking,
-	 * and end date is equal to the start date. The conflicting booking should
-	 * be returned.
-	 * 
-	 * @throws ParseException
-	 */
-	@Test
-	public void returnsBookingIfStartDateBeforeAndEndDateEqualsStart() throws ParseException {
-		// Act.
-		List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 19:00:00"), 30);
+        // Assert.
+        assertEquals(1, result.size());
+    }
 
-		// Assert.
-		assertEquals(1, result.size());
-	}
+    /**
+     * Test the case when start date is before start of a conflicting booking,
+     * and end date is after the start date. The conflicting booking should be
+     * returned.
+     * 
+     * @throws ParseException
+     */
+    @Test
+    public void returnsBookingIfStartDateIsBeforeAndEndIsAfterStart() throws ParseException {
+        // Act.
+        List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 19:15:00"), 30);
 
-	/**
-	 * Test the case when start date is before start of a conflicting booking,
-	 * and end date is after the start date. The conflicting booking should be
-	 * returned.
-	 * 
-	 * @throws ParseException
-	 */
-	@Test
-	public void returnsBookingIfStartDateIsBeforeAndEndIsAfterStart() throws ParseException {
-		// Act.
-		List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 19:15:00"), 30);
+        // Assert.
+        assertEquals(1, result.size());
+    }
 
-		// Assert.
-		assertEquals(1, result.size());
-	}
+    /**
+     * Test the case when start date is on the start date of a conflicting
+     * booking, and end date is after the start date. The conflicting booking
+     * should be returned.
+     * 
+     * @throws ParseException
+     */
+    @Test
+    public void returnsBookingIfStartDateIsEqualAndEndIsAfterStart() throws ParseException {
+        // Act.
+        List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 19:30:00"), 15);
 
-	/**
-	 * Test the case when start date is on the start date of a conflicting
-	 * booking, and end date is after the start date. The conflicting booking
-	 * should be returned.
-	 * 
-	 * @throws ParseException
-	 */
-	@Test
-	public void returnsBookingIfStartDateIsEqualAndEndIsAfterStart() throws ParseException {
-		// Act.
-		List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 19:30:00"), 15);
+        // Assert.
+        assertEquals(1, result.size());
+    }
 
-		// Assert.
-		assertEquals(1, result.size());
-	}
+    /**
+     * Test the case when start date is on the start date of a conflicting
+     * booking, and end date is on the end date. The conflicting booking should
+     * be returned.
+     * 
+     * @throws ParseException
+     */
+    @Test
+    public void returnsBookingIfStartEqualsStartAndEndEqualsEnd() throws ParseException {
+        // Act.
+        List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 19:30:00"), 30);
 
-	/**
-	 * Test the case when start date is on the start date of a conflicting
-	 * booking, and end date is on the end date. The conflicting booking should
-	 * be returned.
-	 * 
-	 * @throws ParseException
-	 */
-	@Test
-	public void returnsBookingIfStartEqualsStartAndEndEqualsEnd() throws ParseException {
-		// Act.
-		List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 19:30:00"), 30);
+        // Assert.
+        assertEquals(1, result.size());
+    }
 
-		// Assert.
-		assertEquals(1, result.size());
-	}
+    /**
+     * Test the case when start date is after the start date of a conflicting
+     * booking, and end date is on the end date. The conflicting booking should
+     * be returned.
+     * 
+     * @throws ParseException
+     */
+    @Test
+    public void returnsBookingIfStartAfterStartAndEndEqualsEnd() throws ParseException {
+        // Act.
+        List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 19:45:00"), 15);
 
-	/**
-	 * Test the case when start date is after the start date of a conflicting
-	 * booking, and end date is on the end date. The conflicting booking should
-	 * be returned.
-	 * 
-	 * @throws ParseException
-	 */
-	@Test
-	public void returnsBookingIfStartAfterStartAndEndEqualsEnd() throws ParseException {
-		// Act.
-		List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 19:45:00"), 15);
+        // Assert.
+        assertEquals(1, result.size());
+    }
 
-		// Assert.
-		assertEquals(1, result.size());
-	}
+    /**
+     * Test the case when start date is after the start date of a conflicting
+     * booking, and end date is after the end date. The conflicting booking
+     * should be returned.
+     * 
+     * @throws ParseException
+     */
+    @Test
+    public void returnsBookingIfStartAfterStartAndEndAfterEnd() throws ParseException {
+        // Act.
+        List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 19:45:00"), 30);
 
-	/**
-	 * Test the case when start date is after the start date of a conflicting
-	 * booking, and end date is after the end date. The conflicting booking
-	 * should be returned.
-	 * 
-	 * @throws ParseException
-	 */
-	@Test
-	public void returnsBookingIfStartAfterStartAndEndAfterEnd() throws ParseException {
-		// Act.
-		List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 19:45:00"), 30);
+        // Assert.
+        assertEquals(1, result.size());
+    }
 
-		// Assert.
-		assertEquals(1, result.size());
-	}
+    /**
+     * Test the case when start date is on the end date of a conflicting
+     * booking, and end date is after the end date. The conflicting booking
+     * should be returned.
+     * 
+     * @throws ParseException
+     */
+    @Test
+    public void returnsBookingIfStartEqualsEndAndEndAfterEnd() throws ParseException {
+        // Act.
+        List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 20:00:00"), 30);
 
-	/**
-	 * Test the case when start date is on the end date of a conflicting
-	 * booking, and end date is after the end date. The conflicting booking
-	 * should be returned.
-	 * 
-	 * @throws ParseException
-	 */
-	@Test
-	public void returnsBookingIfStartEqualsEndAndEndAfterEnd() throws ParseException {
-		// Act.
-		List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 20:00:00"), 30);
+        // Assert.
+        assertEquals(1, result.size());
+    }
 
-		// Assert.
-		assertEquals(1, result.size());
-	}
+    /**
+     * Test the case when start date is after the end date of a conflicting
+     * booking, and end date is after the end date. No conflicting bookings
+     * should be returned.
+     * 
+     * @throws ParseException
+     */
+    @Test
+    public void returnsNothingIfStartAfterEndAndEndAfterEnd() throws ParseException {
+        // Act.
+        List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 20:30:00"), 30);
 
-	/**
-	 * Test the case when start date is after the end date of a conflicting
-	 * booking, and end date is after the end date. No conflicting bookings
-	 * should be returned.
-	 * 
-	 * @throws ParseException
-	 */
-	@Test
-	public void returnsNothingIfStartAfterEndAndEndAfterEnd() throws ParseException {
-		// Act.
-		List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 20:30:00"), 30);
+        // Assert.
+        assertEquals(0, result.size());
+    }
 
-		// Assert.
-		assertEquals(0, result.size());
-	}
+    /**
+     * Test the case when start date is after the start date of a conflicting
+     * booking, and end date is before the end date. The conflicting booking
+     * should be returned.
+     * 
+     * @throws ParseException
+     */
+    @Test
+    public void returnsBookingIfStartAfterStartAndEndBeforeEnd() throws ParseException {
+        // Act.
+        List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 19:40:00"), 10);
 
-	/**
-	 * Test the case when start date is after the start date of a conflicting
-	 * booking, and end date is before the end date. The conflicting booking
-	 * should be returned.
-	 * 
-	 * @throws ParseException
-	 */
-	@Test
-	public void returnsBookingIfStartAfterStartAndEndBeforeEnd() throws ParseException {
-		// Act.
-		List<Spot> result = user.getConflictingSpots(dateFormat.parse("16-11-2020 19:40:00"), 10);
-
-		// Assert.
-		assertEquals(1, result.size());
-	}
+        // Assert.
+        assertEquals(1, result.size());
+    }
 }
